7c48613294c19c3794c128498614e6a64834bf9b
Jasmine Singla <55360377+Jasmine-21@users.noreply.github.com>
2021-02-07 14:50:08 +0530

    Create simplifyPath.py

---
+++ week1/simplifyPath.py
@@ -0,0 +1,11 @@
+class Solution:
+    # @param path, a string
+    # @return a string
+    def simplifyPath(self, path):
+        stack, tokens = [], path.split("/")
+        for token in tokens:
+            if token == ".." and stack:
+                stack.pop()
+            elif token != ".." and token != "." and token:
+                stack.append(token)
+        return "/" + "/".join(stack)
